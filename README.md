a700a550ddf865ad4a2c20a3cbd2aa752d140767
nyg <nyg>
2015-12-19 21:49:56 +0100

    Moved to maven. Print more info about the received request.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # A Minimal Java EE Servlet
 
-1. Modify `JEE_LIBS` and `TOMEE` in `build.sh` and `tomee-deploy.sh` if needed.
-2. Run `build.sh`.
-3. Run `tomee-deploy.sh`.
-4. Go to <http://localhost:8080/MinimalServlet>.
+Run `mvn clean package` to build the WAR.
---
+++ src/main/java/edu/self/MinimalServlet.java
@@ -0,0 +1,55 @@
+package edu.self;
+
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/*")
+public class MinimalServlet implements Servlet {
+
+    private static final Logger logger = Logger.getLogger(MinimalServlet.class.getSimpleName());
+
+    private ServletConfig config;
+
+    @Override
+    public void destroy() {
+        logger.info("Servlet destroyed");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        logger.info("Servlet config requested");
+        return config;
+    }
+
+    @Override
+    public String getServletInfo() {
+        logger.info(" Servlet info requested");
+        return "A Minimal Servlet";
+    }
+
+    @Override
+    public void init(ServletConfig config) {
+        logger.info("Servlet initialized");
+        this.config = config;
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) {
+        for (Method method : ServletRequest.class.getMethods()) {
+            try {
+                String result = method.invoke(request).toString();
+                response.getWriter().println(method.getName());
+                response.getWriter().println("-> " + result + "\n");
+            }
+            catch (Throwable e) {
+                logger.info("Failed on " + method.getName());
+            }
+        }
+    }
+}
