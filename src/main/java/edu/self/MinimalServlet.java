d39980082079962a4500ede83ef5db8d057fa629
nyg <nyg@users.noreply.github.com>
2019-10-06 14:52:02 +0200

    Update pom.xml and use a HttpServlet instead.

--- src/main/java/edu/self/MinimalServlet.java
+++ src/main/java/edu/self/MinimalServlet.java
@@ -1,55 +1,16 @@
 package edu.self;
 
-import java.lang.reflect.Method;
-import java.util.logging.Logger;
-
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/*")
-public class MinimalServlet implements Servlet {
-
-    private static final Logger logger = Logger.getLogger(MinimalServlet.class.getSimpleName());
-
-    private ServletConfig config;
+public class MinimalServlet extends HttpServlet {
 
     @Override
-    public void destroy() {
-        logger.info("Servlet destroyed");
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        logger.info("Servlet config requested");
-        return config;
-    }
-
-    @Override
-    public String getServletInfo() {
-        logger.info("Servlet info requested");
-        return "A Minimal Servlet";
-    }
-
-    @Override
-    public void init(ServletConfig config) {
-        logger.info("Servlet initialized");
-        this.config = config;
-    }
-
-    @Override
-    public void service(ServletRequest request, ServletResponse response) {
-        for (Method method : ServletRequest.class.getMethods()) {
-            try {
-                String result = method.invoke(request).toString();
-                response.getWriter().println(method.getName());
-                response.getWriter().println("-> " + result + "\n");
-            }
-            catch (Throwable e) {
-                logger.info("Failed on " + method.getName());
-            }
-        }
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Hello World!");
     }
 }
